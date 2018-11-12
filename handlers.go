cbac4ef1759009788473356aae8898edc43d94d5
Joseph Timmer <profsmallpine@gmail.com>
2018-11-12 14:09:34 -0700

    Initial work on site index

--- handlers.go
+++ handlers.go
@@ -6,15 +6,17 @@ import (
 	"net/http"
 )
 
-// Handler is http struct for passing services to the router
+// handler is http struct for passing services to the router.
 type handler struct {
 	Logger *log.Logger
 }
 
+// goHome is used for handling requests to "/".
 func (h *handler) goHome(w http.ResponseWriter, r *http.Request) {
 	respond(h.Logger, w, r, "./tmpl/index.tmpl", nil)
 }
 
+// respond is used to parse a base template.
 func respond(logger *log.Logger, w http.ResponseWriter, r *http.Request, layout string, data interface{}) {
 	// Parse static files.
 	tmpl := template.Must(template.New("base.tmpl").ParseFiles(
@@ -23,7 +25,7 @@ func respond(logger *log.Logger, w http.ResponseWriter, r *http.Request, layout 
 	))
 	err := tmpl.Funcs(template.FuncMap{}).Execute(w, data)
 
-	// Log error is compiling template fails.
+	// Log template compilation failure.
 	if err != nil {
 		logger.Println("Template execution error: ", err.Error(), layout)
 		return
